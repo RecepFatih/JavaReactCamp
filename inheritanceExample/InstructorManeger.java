a80bacf8bdbab5a76342f59ab0032222f50cd3f0
Recep Fatih Cantekin <47432750+RecepFatih@users.noreply.github.com>
2022-05-22 14:03:31 +0100

    Update InstructorManeger.java

--- inheritanceExample/InstructorManeger.java
+++ inheritanceExample/InstructorManeger.java
@@ -16,9 +16,5 @@ public class InstructorManeger extends BaseManager{
 				+ "\nInstructor Name: " + instructor.getFirstName() + instructor.getLastName());
 		
 	}
-	
-	public void aba() {
-		
-	}
 
 }
